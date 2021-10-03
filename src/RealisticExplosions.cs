57477e39f88db78e0ce45fe6c2006da630253205
2CHEVSKII <prsroman3@gmail.com>
2021-10-03 06:41:52 +0300

    ReaslisticExplosions v1
    Add config, support for corpses, optimizations, code formating and health

--- src/RealisticExplosions.cs
+++ src/RealisticExplosions.cs
@@ -1,53 +1,222 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+using System;
+using System.Collections.Generic;
 
-/* TODO:
- - Attach to rockets ✔
- - Detect non-exploded grenades/satchels
- */
+using Facepunch;
+
+using JetBrains.Annotations;
+
+using Newtonsoft.Json;
+
+using UnityEngine;
 
 namespace Oxide.Plugins
 {
-	[Info("Realistic Explosions", "2CHEVSKII", "0.2.1")]
-	[Description("Pushes back dropped items when they are near the explosion")]
-	internal class RealisticExplosions : RustPlugin
-	{
+    [Info("Realistic Explosions", "2CHEVSKII", "1.0.0")]
+    [Description("Pushes back dropped items when they are near the explosion")]
+    class RealisticExplosions : CovalencePlugin
+    {
+        static RealisticExplosions Instance;
+        PluginSettings             settings;
 
-		#region -Hooks-
+        #region Oxide hooks
 
+        void Init()
+        {
+            Instance = this;
+            RealisticExplosion.Init();
+        }
 
-		private void OnExplosiveDropped(BasePlayer player, BaseEntity entity, ThrownWeapon item) => entity.gameObject.AddComponent<ExplosionComponent>();
+        void Unload()
+        {
+            RealisticExplosion.Shutdown();
+            Instance = null;
+        }
 
-		private void OnExplosiveThrown(BasePlayer player, BaseEntity entity, ThrownWeapon item) => entity.gameObject.AddComponent<ExplosionComponent>();
+        void OnExplosiveDropped(BasePlayer player, BaseEntity entity)
+        {
+            entity.gameObject.AddComponent<RealisticExplosion>();
+        }
 
-		private void OnRocketLaunched(BasePlayer player, BaseEntity entity) => entity.gameObject.AddComponent<ExplosionComponent>();
+        void OnExplosiveThrown(BasePlayer player, BaseEntity entity)
+        {
+            entity.gameObject.AddComponent<RealisticExplosion>();
+        }
 
+        void OnRocketLaunched(BasePlayer player, BaseEntity entity)
+        {
+            entity.gameObject.AddComponent<RealisticExplosion>();
+        }
 
-		#endregion
+        #endregion
 
-		#region -Component-
+        #region Configuration load
 
+        protected override void LoadDefaultConfig()
+        {
+            settings = PluginSettings.Default;
 
-		private class ExplosionComponent : MonoBehaviour
-		{
-			private BaseEntity Entity { get; set; }
+            SaveConfig();
+        }
 
-			private void Awake() => Entity = GetComponent<BaseEntity>();
+        protected override void LoadConfig()
+        {
+            base.LoadConfig();
 
-			private void OnDestroy()
-			{
-				List<DroppedItem> list = new List<DroppedItem>();
-				Vis.Entities<DroppedItem>(Entity.transform.position, 15f, list);
+            try
+            {
+                settings = Config.ReadObject<PluginSettings>();
 
-				list.RemoveAll(item => item == null || item.IsDestroyed || !item.IsVisible(Entity.transform.position));
+                if (settings == null)
+                {
+                    throw new Exception("Configuration is null");
+                }
+            }
+            catch (Exception e)
+            {
+                LogError("Could not read configuration file:\n{0}", e.Message);
+                LogWarning("Default configuration will be loaded");
 
-				foreach(DroppedItem item in list)
-					item?.GetComponent<Rigidbody>()?.AddExplosionForce(500f, Entity.transform.position, 15f);
-			}
-		}
+                LoadDefaultConfig();
+            }
+        }
 
+        protected override void SaveConfig()
+        {
+            Config.WriteObject(settings);
+        }
 
-		#endregion
+        #endregion
 
-	}
+        [UsedImplicitly]
+        class RealisticExplosion : MonoBehaviour
+        {
+            static List<DroppedItem> ItemList;
+            static List<BaseCorpse>  CorpseList;
+
+            BaseEntity     entity;
+            PluginSettings settings;
+
+            public static void Init()
+            {
+                ItemList = Pool.GetList<DroppedItem>();
+                CorpseList = Pool.GetList<BaseCorpse>();
+            }
+
+            public static void Shutdown()
+            {
+                Pool.Free(ref ItemList);
+                Pool.Free(ref CorpseList);
+            }
+
+            void Awake()
+            {
+                settings = Instance.settings;
+                entity = GetComponent<BaseEntity>();
+            }
+
+            void OnDestroy()
+            {
+                CollectItems();
+                CollectCorpses();
+
+                IterateEntities();
+
+                Cleanup();
+            }
+
+            void IterateEntities()
+            {
+                for (var i = 0; i < ItemList.Count; i++)
+                {
+                    DroppedItem item = ItemList[i];
+
+                    if (settings.CheckVisibility && !IsVisible(item))
+                    {
+                        continue;
+                    }
+
+                    Rigidbody rb = item.GetComponent<Rigidbody>();
+
+                    ApplyForce(rb);
+                }
+
+                for (var i = 0; i < CorpseList.Count; i++)
+                {
+                    BaseCorpse corpse = CorpseList[i];
+
+                    if (settings.CheckVisibility && !IsVisible(corpse))
+                    {
+                        continue;
+                    }
+
+                    Rigidbody rb = corpse.GetComponent<Rigidbody>();
+
+                    ApplyForce(rb, true);
+                }
+            }
+
+            void Cleanup()
+            {
+                ItemList.Clear();
+                CorpseList.Clear();
+            }
+
+            void CollectItems()
+            {
+                if (!settings.AffectDroppedItems)
+                {
+                    return;
+                }
+
+                Vis.Entities(entity.transform.position, settings.ExplosionRadius, ItemList);
+            }
+
+            void CollectCorpses()
+            {
+                if (!settings.AffectRagdolls)
+                {
+                    return;
+                }
+
+                Vis.Entities(entity.transform.position, settings.ExplosionRadius, CorpseList);
+            }
+
+            void ApplyForce(Rigidbody rigidbody, bool isCorpse = false)
+            {
+                rigidbody.AddExplosionForce(
+                    isCorpse ? settings.ExplosionForce * 5f : settings.ExplosionForce,
+                    entity.transform.position,
+                    settings.ExplosionRadius
+                );
+            }
+
+            bool IsVisible(BaseEntity baseEntity)
+            {
+                return !settings.CheckVisibility || baseEntity.IsVisible(entity.transform.position + new Vector3(0, 0.5f));
+            }
+        }
+
+        class PluginSettings
+        {
+            public static PluginSettings Default =>
+                new PluginSettings {
+                    ExplosionRadius = 15f,
+                    ExplosionForce = 500f,
+                    CheckVisibility = false,
+                    AffectDroppedItems = true,
+                    AffectRagdolls = true
+                };
+
+            [JsonProperty("Explosion force")]
+            public float ExplosionForce { get; set; }
+            [JsonProperty("Explosion radius")]
+            public float ExplosionRadius { get; set; }
+            [JsonProperty("Check visibility from explosion to object (less performant)")]
+            public bool CheckVisibility { get; set; }
+            [JsonProperty("Affect ragdolls")]
+            public bool AffectRagdolls { get; set; }
+            [JsonProperty("Affect dropped items")]
+            public bool AffectDroppedItems { get; set; }
+        }
+    }
 }
